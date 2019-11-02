23396244f68719f0e19701418f05c7b774a55bf9
Ami Ikanovic <ikanovic@iastate.edu>
2019-11-01 21:15:17 -0500

    Added intent filter

---
+++ app/src/main/java/com/example/hackathon2019/splashActivity.java
@@ -0,0 +1,14 @@
+package com.example.hackathon2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
